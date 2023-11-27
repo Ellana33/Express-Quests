05333d8d9d12652bba9ecb367f84edb3c72d0d88
Anaelle Cauvin <anaelle.cauvin@epitech.eu>
2023-11-27 10:42:15 +0100

    [ADD] quest 2

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+require("dotenv").config();
+const port = process.env.APP_PORT;
 
 const app = express();
 
@@ -6,5 +8,53 @@ const movieControllers = require("./controllers/movieControllers");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
+
+app.get("/api/users", (req, res) => {
+    const users = [
+        {
+            id: 1,
+            name: "John",
+            email: "john@gmail.com"
+        },
+        {
+            id: 2,
+            name: "Bob",
+            email: "bob@gmail.com"
+        },
+        {
+            id: 3,
+            name: "Shannon",
+            email: "shannon@gmail.com"
+        },
+    ];
+    res.status(200).json(users);
+});
+
+app.get("/api/users/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const users = [
+        {
+            id: 1,
+            name: "John",
+            email: "john@gmail.com"
+        },
+        {
+            id: 2,
+            name: "Bob",
+            email: "bob@gmail.com"
+        },
+        {
+            id: 3,
+            name: "Shannon",
+            email: "shannon@gmail.com"
+        },
+    ];
+    const user = users.find(user => user.id === id);
+    if (user) {
+        res.status(200).json(user);
+    } else {
+        res.sendStatus(404);
+    }
+});
 
 module.exports = app;
